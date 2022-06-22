c04b089667083c2f4f6fa3b812bfd7ca6becd4d0
aparnageorge <aparnageorge94@gmail.com>
2022-06-22 16:30:26 +0100

    astarPathfinder

---
+++ astarPathfinder.py
@@ -0,0 +1,74 @@
+import numpy
+from heapq import *
+# heap used to implement priorityqueue
+
+# to return expected distance. Since diagonal movement is possible, 
+# euclidean distance is considered here instead of manhattan distance
+def heuristic(a, b):
+    return numpy.sqrt((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2)
+
+def route(array, start, end, obstacles):
+
+    neighbors = [(0,1),(0,-1),(1,0),(-1,0),(1,1),(1,-1),(-1,1),(-1,-1)]
+# set to store elements that were already covered/need not be considered again
+    close_set = set()
+# to backtrack the path covered- contains all covered routes in each iteration
+    path = {}
+# distance from start
+    gscore = {start:0}
+# total expected distance to end
+    fscore = {start:heuristic(start, end)}
+    open_heap = []
+
+    heappush(open_heap, (fscore[start], start))
+    
+    while open_heap:
+        # extract neighbour with least f score
+        current = heappop(open_heap)[1]
+
+        if current == end:
+            shortest_route = []
+            while current in path:
+                shortest_route.append(current)
+                current = path[current]
+            return shortest_route[::-1]
+        #add current position to covered set
+        close_set.add(current)
+        #loop over neighbors to find their g scores
+        for i, j in neighbors:
+            neighbor = current[0] + i, current[1] + j      
+            expected_gscore = gscore[current] + heuristic(current, neighbor)
+            # eliminate invalid positions and obstacles
+            if 0 <= neighbor[0] < array.shape[0]:
+                if 0 <= neighbor[1] < array.shape[1]:                
+                    if (neighbor[0],neighbor[1]) in obstacles:
+                        continue
+                else:
+                    continue
+            else:
+                continue
+            
+            # neighbor already considered but expected score via updated route is larger than score earlier found
+            if neighbor in close_set and expected_gscore >= gscore.get(neighbor, 0):
+                continue
+            
+            # expected score via updated route is smaller than score earlier found or found new position not already
+            # in open set
+            if  expected_gscore < gscore.get(neighbor, 0) or neighbor not in [i[1]for i in open_heap]:
+                path[neighbor] = current
+                gscore[neighbor] = expected_gscore
+                fscore[neighbor] = expected_gscore + heuristic(neighbor, end)
+                heappush(open_heap, (fscore[neighbor], neighbor))
+                
+    return False
+
+nmap = numpy.zeros((10,10))
+# assuming obstacles are at (7,7) and (7,8) according to diagram instead of phase 1 text.
+obstacles = [(9,7),(8,7),(7,7),(7,8)]
+    
+route = route(nmap, (0,0), (9,9), obstacles)
+route = [(0,0)] + route
+
+
+print("Path =" , route)
+print("Number of steps taken from (0,0) =" , len(route)-1)
